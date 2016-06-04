014d7e8c7ae02eef9cc9940833dfcd5b4f49ced7
Nikita <nks.off@gmail.com>
2016-06-04 06:31:49 +0300

    cache '/' in service worker

--- assets/js/offline.js
+++ assets/js/offline.js
@@ -53,6 +53,7 @@ this.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
+        '/',
         '/favicon.ico',
         '/css/main.min.css',
         '/js/main.min.js',
